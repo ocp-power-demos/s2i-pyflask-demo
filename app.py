3100f048c3fd59939edff06de39aeb74c52cda30
Deepak C Shetty <dpkshetty@gmail.com>
2021-02-03 19:53:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World !'
+
+@app.route('/version')
+def get_version():
+    return 'App version : <b>1.0</b>'
+
+@app.route('/test')
+def get_test():
+    return 'You are accessing /test endpoint'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
