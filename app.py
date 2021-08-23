ce84cbd398e6e3eeeabc17af10e92f1150196a39
Deepak C Shetty <dpkshetty@gmail.com>
2021-08-23 12:38:13 +0530

    Added html h1 tags

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World ! - Pyflask Demo'
+    return '<h1>Hello, World ! - Pyflask Demo</h1>'
 
 @app.route('/version')
 def get_version():
-    return 'App version : <b>1.0</b>'
+    return '<h1>App version : <b>1.0</b></h1>'
 
 @app.route('/test')
 def get_test():
-    return 'You are accessing /test endpoint'
+    return '<h1>You are accessing /test endpoint</h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
